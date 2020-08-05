93baf271ad2f3c15035a07623d7afc70a0582e3b
Alexander Gonzalez Fernandez <alexander.gonzalez@ucreativa.com>
2020-08-04 20:06:22 -0600

    Agregando expects

---
+++ test/pageobjects/dashboard.page.js
@@ -0,0 +1,29 @@
+const Page = require('./page');
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class DashboardPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get noArticlesLabel () { return $('div=No articles are here... yet.') }
+    get yourFeedTap () { return $('.nav-link=Your Feed')}
+    get globalFeedTap () {return $('.nav-link=Global Feed')}
+    
+
+    getNoArticlesLabel() {
+        return this.noArticlesLabel;
+    }
+
+    getYourFeedTap () {
+        return this.yourFeedTap;
+    }
+
+    getGlobalFeedTap() {
+        return this.globalFeedTap;
+    }
+
+}
+
+module.exports = new DashboardPage();
--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,13 +1,28 @@
 const LoginPage = require('../pageobjects/login.page');
 const NavigationPage = require('../pageobjects/navigation.page');
+const DashboardPage = require('../pageobjects/dashboard.page')
 
 describe('My Login application', () => {
     it('should login with valid credentials', () => {
         NavigationPage.open();
         NavigationPage.goToSignIn();
         browser.pause(5000);
-        LoginPage.login('Hola', '123');
+        LoginPage.login('alex.gonzalez@ucreativa.com', 'Test1234');
+        expect(browser).toHaveUrl('https://demo.realworld.io/#/');
+        expect(DashboardPage.getNoArticlesLabel()).toBeDisplayed();
+        expect(DashboardPage.getYourFeedTap()).toHaveAttribute('class', 'nav-link active')
     });
+
+    it('should open Global Feed Tab',() => {
+        DashboardPage.getGlobalFeedTap().click();
+       // expect(DashboardPage.getGlobalFeedTap()).toHaveAttribute('class', 'nav-link active');
+        expect(DashboardPage.getGlobalFeedTap()).toHaveClass('active', { message: 'Not active tap!', })
+     //  expect(DashboardPage.getGlobalFeedTap()).toHaveProperty('class', 'nav-link active');
+     expect(DashboardPage.getGlobalFeedTap()).toBeClickable();
+     expect(DashboardPage.getGlobalFeedTap()).toBeSelected();
+     
+    });
+
 });
 
 
